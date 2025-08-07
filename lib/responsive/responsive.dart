92128789c3fab0d06c795bf34fbc4d0736a87e86
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-08-07 14:32:18 +0300

    correct wrong spell file

--- lib/responsive/responsive.dart
+++ lib/responsive/responsive.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_design/responsive/dimestion.dart';
+import 'package:responsive_design/responsive/dimension.dart';
 
 class ResponsiveLayout extends StatelessWidget {
   final Widget mobileBody;
